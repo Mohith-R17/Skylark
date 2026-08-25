1850dd5e025f8c160290214499749f1677de023c
mohith-r17 <mohithr4567@gmail.com>
2026-08-25 14:35:58 +0530

    Fix CORS policy to allow render frontend

--- backend/app.py
+++ backend/app.py
@@ -42,7 +42,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://localhost:3000", 
+        "https://skylark-frontend-uuyg.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
